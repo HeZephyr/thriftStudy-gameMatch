49de4ebb1d32975d08f274a06c68069041413740
hzf <unique.hzf@gmail.com>
2022-01-29 15:41:01 +0800

    first finish match_server

---
+++ thrift/match.thrift
@@ -0,0 +1,25 @@
+# C++命令空间
+namespace cpp match_service
+
+# 用户类
+struct User{
+1: i32 id,
+       2: string name,
+       3: i32 score
+}
+service Match{
+    /**
+     * user：添加的用户信息
+     * info：附加信息
+     * 作用：向匹配池中添加一名用户
+     *
+     */
+    i32 add_user(1: User user, 2: string info),
+    /**
+     * user：删除的用户信息
+     * info：附加信息
+     * 作用：向匹配池中删除一名用户
+     *
+     */
+    i32 remove_user(1: User user, 2: string info),
+}
